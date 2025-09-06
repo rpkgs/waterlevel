2e38c13bee8e0f261fd321043a1019aec27992cf
Dongdong Kong <kongdd@users.noreply.github.com>
2025-09-06 19:37:46 +0800

    add ci

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // harmonic
 Rcpp::NumericMatrix harmonic(arma::vec times, arma::vec& freq);
 RcppExport SEXP _waterlevel_harmonic(SEXP timesSEXP, SEXP freqSEXP) {
